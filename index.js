413af994bc63d15640220d9daf41e36316cba241
Illi <107838600+justlsia@users.noreply.github.com>
2024-10-10 12:43:52 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+/**
+* Serveur Backend Pokedex
+*/
+//console.log ("Hello World!");
+// Définir l'emplacement des fichiers bases de données
+const POKEDEX_SRC = "./DATA/pokedex.json";
+
+// Définir l'emplacement des images
+const IMAGES_SRC = "./FILES/images";
+
+// Définir un port
+const PORT = 5001;
+
+
+// ************************************************
+
+// Lancer un serveur express sur un port défini
+const fs = require('fs');
+
+// npm install express
+const express = require('express');
+const app = express();
+
+// Lancement du serveur et attendre
+app.listen(
+    PORT,
+    '127.0.0.1',
+    () => {
+        console.log('Server Pokedex is listening on ' + PORT);
+    }
+)
+
+
+// Créer les routes
+
+// Créer le chemin racine
+app.all('/', (req, res, next) => {
+    console.log('Accessing the root page ...');
+    next() // pass control to the next handler
+  })
+
+// Créer le chemin hasard
+app.all('/hasard', (req, res, next) => {
+    console.log('Accessing the hasard page ...');
+    next() // pass control to the next handler
+  })
+
+
+// Afficher le chemin racine à l'accueil du site
+// GET method route
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur la page principales');
+  })
